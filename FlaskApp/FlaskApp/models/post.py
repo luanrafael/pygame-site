5f5607ac3680686b63a64ac600df27391458c90a
iuryalves <iuryalves20@gmail.com>
2014-06-27 00:54:51 -0300

    documentando arquivo do model

--- FlaskApp/FlaskApp/models/post.py
+++ FlaskApp/FlaskApp/models/post.py
@@ -3,7 +3,14 @@
 from sqlobject import  StringCol, DateTimeCol
 from model_base import *
 
+__author__ = 'iury'
+
 class Post(ModelBase):
+    '''
+    Class for store posts
+    To create a new Post object just call Post(author=author, content=content, title=title)
+    you don't need to pass the field date, it is create automatically
+    '''
     author = StringCol()
     content = StringCol()
     title = StringCol()
@@ -11,6 +18,9 @@ class Post(ModelBase):
     
 
     def to_dict(self):
+        '''
+        return a dict of the fields in the class plus the id field, which is a unique field for each object
+        '''
         return {
             'author': self.author,
             'content': self.content,
@@ -21,17 +31,25 @@ class Post(ModelBase):
 
     @classmethod
     def save_post(cls, author, content, title):
+        '''
+        Save a post and return the post object, must call as classmethod
+        '''
         post = cls(author=author, content=content, title=title)
         return post
 
     @classmethod
     def delete_post(cls, id):
+        '''
+        delete a post of a given id, must run as classmethod
+        '''
         cls.delete(id)
 
     @classmethod
     def edit_post(cls, id, dic):
+        '''
+        Edit a post of a given id, must run as classmethod
+        '''
         post = cls.get(id)
-
         post.author = dic['author']
         post.content = dic['content']
         post.title = dic['title']
