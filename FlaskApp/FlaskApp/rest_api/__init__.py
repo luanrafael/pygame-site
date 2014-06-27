100c66745e6bd31d6f51e446bcd2d0d921b198cd
iuryalves <iuryalves20@gmail.com>
2014-06-27 01:09:31 -0300

    documentando rest apli [parcial]

--- FlaskApp/FlaskApp/rest_api/__init__.py
+++ FlaskApp/FlaskApp/rest_api/__init__.py
@@ -1,3 +1,27 @@
+'''
+Module containing all rest apis
+Each rest api must be register in the __main__ file with the method register_blueprint
+the rest api must follow this base structure
+
+    from flask import Blueprint, jsonify
+
+    api_name = Blueprint("api_name", __name__)
+
+    @api_name.route("/url_prefix/endpoint", methods=["GET"])
+    def function():
+        return jsonify({"hello": "world"})
+
+
+The method route receive two params:
+    -> a string which is the url to map
+    -> a list of allowed methods for the request, commonly used are 'POST', 'GET', 'PUT', 'DELETE'
+
+url_prefix is the common url for all routes in the file
+endpoint is a suggestive for the operation that the function does
+
+each function must have a suggestive name and  must return a json, using the function jsonify
+'''
+
 __author__ = 'iury'
 
 
--- FlaskApp/FlaskApp/rest_api/posts_rest_api.py
+++ FlaskApp/FlaskApp/rest_api/posts_rest_api.py
@@ -1,20 +1,18 @@
 from flask import Blueprint, jsonify, request
 from FlaskApp.models.post import Post
-from FlaskApp.models.db_config import init_db
 
 __author__ = 'iury'
 
-init_db()
 posts_api = Blueprint("posts_api", __name__)
 
-@posts_api.route("/get_all_posts", methods=["GET"])
+@posts_api.route("/posts/get_all_posts", methods=["GET"])
 def get_all_posts():
     posts = Post.select()
     posts = [post.to_dict() for post in posts]
     return jsonify({'posts': posts})
 
 
-@posts_api.route("/add_post", methods=["POST"])
+@posts_api.route("/posts/add_post", methods=["POST"])
 def add_post():
     author = request.json['author']
     content = request.json['content']
@@ -24,14 +22,14 @@ def add_post():
     return jsonify(request.json)
 
 
-@posts_api.route("/delete_post", methods=["POST"])
+@posts_api.route("/posts/delete_post", methods=["POST"])
 def delete_post():
     id = request.json['id']
     Post.delete_post(id)
     return jsonify({})
 
 
-@posts_api.route("/edit_post", methods=["POST"])
+@posts_api.route("/posts/edit_post", methods=["POST"])
 def edit_post():
     id = request.json.pop('id')
     Post.edit_post(id, request.json)
