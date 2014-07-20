0c473d9efb92a3a783801961d3e7815b31f9e5b0
Iury Alves <satriani-16@gmail.com>
2014-07-20 20:02:11 -0300

    concertando falha no html

--- FlaskApp/FlaskApp/static/js/src/filters/filters.js
+++ FlaskApp/FlaskApp/static/js/src/filters/filters.js
@@ -1,6 +1,8 @@
 app.filter('sliceContent', function(){
 
+	var html = '<a class="link" ng-click="showEntirePost()">Leia Mais</a>';
+
 	return function(input){
-		return input.slice(0, 400);
+		return input.slice(0, 400) + '...' + html;
 	};
 });
