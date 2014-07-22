c8f2115b5bf643140c614fa03030e32e076c7060
iury <satriani-16@gmail.com>
2014-07-22 00:45:02 -0300

    commit parcial da navbar de categorias

--- FlaskApp/FlaskApp/static/js/src/controllers/home_page_controller.js
+++ FlaskApp/FlaskApp/static/js/src/controllers/home_page_controller.js
@@ -1,13 +1,15 @@
-
+	
 
 app.controller('homePageCtrl', ['$scope', '$window', 'posts_rest_api', function($scope, $window, posts_rest_api){
 
 	$scope.posts = [];
+	$scope.posts_per_categorie = {'pygame': 0, 'pysdl': 0, 'pyj2d': 0};
 
 	$window.onload = function(){
 		posts_rest_api.get_all_posts().success(function(result){
 			$scope.posts = result.posts.reverse();
 			_transform_to_date($scope.posts);
+			_count_post_categories();
 		}).error(function(err){
 			console.log(err);
 		});
@@ -16,10 +18,25 @@ app.controller('homePageCtrl', ['$scope', '$window', 'posts_rest_api', function(
 	$window.onscroll = function(){
 		if ($window.pageYOffset > 165){}
 	};
+	$scope.changeTextColor = function(id, color){
+		var element = document.getElementById(id);
+		element.style.color = color;
+	};
+
+	$scope.setCategorie = function(categorie){
+		$scope.categorie = categorie;
+	};
 
 	var _transform_to_date = function(list){
 		for (var item =  0; item < list.length; item++){
 			list[item].date = new Date(Date.parse(list[item].date));
+		}
+	};
+	var _count_post_categories = function(){
+		for (var i in $scope.posts){
+			attr = $scope.posts[i].categorie;
+			if (attr)
+				$scope.posts_per_categorie[attr.toLowerCase()] ++;
 		}
 	};
 }]);
