fa5a1ef4e6932e1386848bb4bf96cd391d3ca8a0
iury <satriani-16@gmail.com>
2014-07-26 20:17:45 -0300

    adicionando directiva de loader

--- FlaskApp/FlaskApp/static/js/src/directives/loaders/loader_directive.js
+++ FlaskApp/FlaskApp/static/js/src/directives/loaders/loader_directive.js
@@ -2,9 +2,6 @@ app.directive("loader", function(){
 	return{
 		restrict: "E",
 		replace: true,
-		templateUrl: '/static/js/src/directives/loaders/loader.html',
-		link: function(){
-			
-		}
+		templateUrl: '/static/js/src/directives/loaders/loader.html'
 	};
 });
