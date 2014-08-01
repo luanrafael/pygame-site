cf216ef28b0eefb702c6a7772b51d5a49c76a8ed
iuryAlves <satriani-16@hotmail.com>
2014-08-01 09:38:50 -0300

    criando arquivo para inicializar databases de teste

--- FlaskApp/test/init_test_db.py
+++ FlaskApp/test/init_test_db.py
@@ -15,19 +15,43 @@ config_connection = {
 	"passwd": "pygame"
 }
 
-cursor = None
 
-def create_test_database():
-	global cursor
+def connect_to_database_server():
+	"""
+	Connect to a database server, 
+	using the config_connection dict
+	:Return: a cursor to the connection or None
+	"""
+	logging.info("trying to connect to the server")
+	cursor = MySQLdb.connect(**config_connection).cursor()
+	return cursor
 
-	logging.info("trying to connect to the database %s" %DATABASE_NAME)
-	cursor = MySQLdb.connect(**config_connection)
+def create_test_database(cursor, name):
+	"""
+	Create a test database with the connection 
+	represented by cursor and name represented by
+	name. If the database already exists  then 
+	the database is deleted and created again
+	"""
+	
+	logging.info("trying to create the database %s" %name)
+	try:
+		cursor.execute("CREATE DATABASE %s" %name)
+	except ProgrammingError:
+		logging.info("database %s exists!" %name)
+		destroy_test_database(name)
+		cursor.execute("CREATE DATABASE %s" %name)
 
-	logging.info("trying to create the database %s" %DATABASE_NAME)
-	cursor.execute("CREATE DATABASE %s" %DATABASE_NAME)
+def destroy_test_database(cursor, name):
+	"""
+	Destroy the test database if the 
+	database exists
+	"""
+	logging.info("trying to delete the database: %s" %name)
+	cursor.execute("DROP DATABASE IF EXISTS %" %name)
 
-def destroy_test_database():
-	global cursor
 
-	logging.info("trying to delete the database: %s" %DATABASE_NAME)
-	cursor.execute("DROP DATABASE IF EXISTS %" %DATABASE_NAME)
+if __name__ == '__main__':
+	cursor = connect_to_database(DATABASE_NAME)
+	if cursor is not None:
+		create_test_database(cursor, DATABASE_NAME)
