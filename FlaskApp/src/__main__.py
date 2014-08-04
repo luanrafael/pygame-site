5201d196421249aee3fe1987e1ca614b071fcc44
iuryAlves <satriani-16@hotmail.com>
2014-08-04 00:20:36 -0300

    pequena correcao no login de usuarios

--- FlaskApp/src/__main__.py
+++ FlaskApp/src/__main__.py
@@ -25,8 +25,8 @@ login_manager.init_app(app)
 
 
 @login_manager.user_loader
-def load_user(id):
-    return User.get(1)
+def load_user(user_id):
+    return User.get(user_id)
 
 blueprints = [
     home_views.view, downloads_views.view, docs_views.view,
