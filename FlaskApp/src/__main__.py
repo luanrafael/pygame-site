11e9ffcf9f8a36aaa9d71b4dca6a3c37ddda2bdb
iuryAlves <satriani-16@hotmail.com>
2014-08-19 07:50:56 -0300

    refactoring

--- FlaskApp/src/__main__.py
+++ FlaskApp/src/__main__.py
@@ -4,11 +4,7 @@ put_project_in_path()
 
 from flask import Flask
 from flask_login import LoginManager
-from views import home_views, downloads_views, login
-from views import docs_views
-from views import forum_views
-from views import projects_views
-from views import admin_views
+from views import (home, downloads, login, docs, forum, projects, admin)
 from rest_api import posts_rest_api, contents_rest_api
 from src import config
 from db_config import init_db
@@ -29,10 +25,9 @@ def load_user(user_id):
     return User.get(user_id)
 
 blueprints = [
-    home_views.view, downloads_views.view, docs_views.view,
-    forum_views.view, projects_views.view,
-    admin_views.view, posts_rest_api.posts_api, login.view,
-    contents_rest_api.contents_api
+    home.view, downloads.view, docs.view, forum.view, projects.view,
+    admin.view, posts_rest_api.posts_api, login.view,
+    contents_rest_api.contents_api,
 ]
 
 map(app.register_blueprint, blueprints)
